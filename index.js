4e04aa92a88eddd81db0d8a13c06c6e29654b6a4
Harjaap Dhillon <harjaapdhillon@Harjaaps-MacBook-Pro.local>
2023-07-31 19:19:47 +0530

    changes

--- index.js
+++ index.js
@@ -1,12 +1,40 @@
-const http = require('http');
-const PORT = 3000;
+const express = require("express");
+const cors = require("cors");
+const { urlencoded, json } = require("body-parser");
+const app = express();
+const qrcode = require("qrcode");
 
-const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello World!');
+app.use(urlencoded({ extended: false }));
+app.use(express.json({ extended: true }));
+app.use(json());
+app.use(cors({ origin: true, methods: ["POST"] }));
+app.use(express.json({ limit: "100mb" }));
+app.use(express.urlencoded({ limit: "100mb" }));
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Methods",
+    "GET, POST, OPTIONS, PUT, PATCH, DELETE"
+  );
+  res.header(
+    "Access-Control-Allow-Headers",
+    "x-access-token, Origin, X-Requested-With, Content-Type, Accept"
+  );
+  console.log(req.path);
+  next();
 });
 
-server.listen(PORT, () => {
-  console.log(`Server running at http://localhost:${PORT}/`);
+app.get("/getbase64image?", (req, res) => {
+  const { data } = req.query;
+  qrcode.toDataURL(data, (err, base64QRCode) => {
+    if (err) throw err;
+    console.log(base64QRCode);
+    res.send({ qrcodedata: base64QRCode });
+  });
+});
+
+const port = process.env.PORT || 3000;
+
+app.listen(port, () => {
+  console.log("app running on port " + port);
 });
